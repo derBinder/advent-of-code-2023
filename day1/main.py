2f391f1619d61235a1c82293f5411559a10f4dee
Felix Binder <felixbinder123@gmx.de>
2023-12-01 12:33:34 +0100

    solve day 1 - part 2

--- day1/main.py
+++ day1/main.py
@@ -1,17 +1,54 @@
-if __name__ == '__main__':
-    file = open('day1/input.in').read()
-    rows = file.split('\n')
+file = open('day1/input.in').read()
+rows = file.split('\n')
+writtenNumbers = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+
+def part1():
     result = 0
 
     for row in rows:
-        calibrationValues = ''
         numbers = []
 
         for char in list(row):
             if char.isnumeric():
                 numbers.append(char)
 
-        calibrationValues = numbers[0] + numbers[-1]
-        result = result + int(calibrationValues)
+        result += int(numbers[0] + numbers[-1])
+    return result
 
-    print(result)
+
+def part2():
+    result = 0
+
+    for row in rows:
+        numbers = []
+        buffer = ''
+
+        for char in list(row):
+            if char.isnumeric():
+                numbers.append(char)
+
+            buffer += char
+
+            for key, value in writtenNumbers.items():
+                if buffer.find(key) != -1:
+                    numbers.append(value)
+                    buffer = buffer[buffer.find(key) + len(key) - 1:]
+
+        result += int(numbers[0] + numbers[-1])
+    return result
+
+
+if __name__ == '__main__':
+    print('Part 1:', part1())
+    print('Part 2:', part2())
