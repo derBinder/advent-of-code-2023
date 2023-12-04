409d724822e56402548745d2647934db562aa19b
Felix Binder <felixbinder123@gmx.de>
2023-12-04 11:32:43 +0100

    resolve day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -33,5 +33,32 @@ def part1():
     return id_sum
 
 
+def part2():
+    total_power = 0
+
+    for game in rows:
+        game_sets = game.split(':')[1].split(';')
+        max_needed_cubes = {}
+        power = 1
+
+        for game_set in game_sets:
+            colors = game_set.split(',')
+
+            for color in colors:
+                color = color.strip()
+                count = int(color.split(' ')[0])
+                color_name = color.split(' ')[1]
+
+                if max_needed_cubes.get(color_name) is None or max_needed_cubes.get(color_name) < count:
+                    max_needed_cubes[color_name] = count
+
+        for color in max_needed_cubes.items():
+            power *= int(color[1])
+
+        total_power += power
+    return total_power
+
+
 if __name__ == '__main__':
     print('Part 1:', part1())
+    print('Part 2:', part2())
